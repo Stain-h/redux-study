617dda1e98b518ed8ded99ddc0275b1d90a3f238
Stain-h <sangheon1646@naver.com>
2021-10-02 08:36:12 +0900

    reducer setting

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,3 +6,8 @@ function addTodo(todo) {
     todo
   }
 }
+
+/*
+  
+
+*/
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,16 +1,23 @@
-import { ADD_TODO } from './actions'
-// state
-// ['코딩', '점심먹기']
+import {ADD_TODO} from './actions'
+// state ['코딩', '점심먹기']
 const initialState = [];
-function todoApp(previousState = initialState, action) {
-  // 초기값 설정 
-  // if(previousState == undefined){
-  //   return [];
-  // }
+export function todoApp(previousState = initialState, action) {
+    // 초기값 설정 if(previousState == undefined){   return []; }
 
-  if(action.type ===  ADD_TODO){
-    return [ ... previousState, action.todo];
-  }
+    if (action.type === ADD_TODO) {
+        return [
+            ...previousState,
+            action.todo
+        ];
+    }
 
-  return previousState;
+    return previousState;
 }
+
+/*
+		Reducer 란?
+		현재 상태와 액션 객체를
+		파라미터로 받아와서
+		새로운 상태를 반환해주는
+		함수이다.
+ */
