647b6ac0f14a5fb875485fffcbe08178db51dd05
Stain-h <sangheon1646@naver.com>
2021-10-16 15:42:20 +0900

    add : redux&context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useContext} from 'react'
+import { addTodo } from './redux/actions'
+import ReduxContext from './context/ReduxContext';
+
+function useReduxState() {
+  const store = useContext(ReduxContext);
+
+  const [state, setState] = useState(store.getState())
+  
+  useEffect(() => {
+    const unsubscribe = store.subscribe(()=> {
+      setState(store.getState());
+    })
+    return () => {
+      unsubscribe();
+    }
+  }, [store]);
+
+  return state;
+}
+
+function useReduxDispatch() {
+  const store = useContext(ReduxContext);
+   
+  return store.dispatch;
+}
+
 
 function App() {
+  const state = useReduxState();
+  const dispatch = useReduxDispatch();
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {JSON.stringify(state)} 
+        <button onClick={click}>추가</button>
       </header>
     </div>
   );
+  function click() {
+    dispatch(addTodo('todo'));
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './redux/store'
-import {addTodo, completeTodo, showAll, showComplete} from './redux/actions'
+import ReduxContext from './context/ReduxContext';
+// import {addTodo, completeTodo, showAll, showComplete} from './redux/actions'
 
-store.subscribe(()=> {
-  console.log(store.getState())
-})
+// store.subscribe(()=> {
+//   console.log(store.getState())
+// })
 
-store.dispatch(addTodo('할일'))
-store.dispatch(completeTodo(0))
-store.dispatch(showComplete())
+// store.dispatch(addTodo('할일'))
+// store.dispatch(completeTodo(0))
+// store.dispatch(showComplete())
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxContext.Provider value={store}>
+      <App />
+    </ReduxContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
