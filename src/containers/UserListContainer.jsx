d8f348b67f683286a71126bebc0b595615c5daaf
Stain-h <sangheon1646@naver.com>
2021-11-06 21:57:32 +0900

    modify : redux-saga

--- src/containers/UserListContainer.jsx
+++ src/containers/UserListContainer.jsx
@@ -1,14 +1,14 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserList from "../components/UserList";
-import { getUsersThunk, getUsersPromise} from "../redux/modules/users";
+import { getUsersThunk, getUsersPromise, getUsersSagaStart} from "../redux/modules/users";
 
 export default function UserListContainer(){
   const users = useSelector(state => state.users.data)
   const dispatch = useDispatch();
 
   const getUsers = useCallback(() => {
-    dispatch(getUsersThunk());
+    dispatch(getUsersSagaStart());
   }, [dispatch])
 
   return <UserList users={users} getUsers={getUsers} />
--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
+import {push} from 'connected-react-router';
+import {put, call, delay, takeEvery} from 'redux-saga/effects';
 
-// 액션 타입 
-
-// 깃헙 API 호출 시작을 의미
+// 액션 타입 깃헙 API 호출 시작을 의미
 export const GET_USER_START = 'redux-study/users/GET_USER_START';
 // 깃헙 API 호출에 대한 응답이 성공적으로 돌아온 경우
 export const GET_USER_SUCCESS = 'redux-study/users/GET_USER_SUCCESS';
@@ -18,100 +18,125 @@ const GET_USERS_REJECTED = 'redux-study/users/GET_USERS_REJECTED';
 
 // 액션 생성 함수
 export function getUsersStart() {
-  return {type: GET_USER_START}
+    return {type: GET_USER_START}
 }
 export function getUsersSuccess(data) {
-  return {type: GET_USER_SUCCESS, data}
+    return {type: GET_USER_SUCCESS, data}
 }
 export function getUsersFail(error) {
-  return {type: GET_USER_FAIL, error}
+    return {type: GET_USER_FAIL, error}
 }
 
-// 초기값 
+// 초기값
 const initialState = {
-  loading: false,
-  data: [],
-  error: null
+    loading: false,
+    data: [],
+    error: null
 };
 
-// 리듀서 
+// 리듀서
 export default function reducer(state = initialState, action) {
-  if(action.type === GET_USER_START || action.type === GET_USERS_PENDING){
-    return {
-      ...state,
-      loading: true,
-      error: null
+    if (action.type === GET_USER_START || action.type === GET_USERS_PENDING) {
+        return {
+            ...state,
+            loading: true,
+            error: null
+        }
     }
-  }
 
-  if(action.type === GET_USER_SUCCESS){
-    return {
-      ...state,
-      loading: false,
-      data: action.data
+    if (action.type === GET_USER_SUCCESS) {
+        return {
+            ...state,
+            loading: false,
+            data: action.data
+        }
     }
-  } 
 
-  if(action.type === GET_USERS_FULFILLED){
-    return {
-      ...state,
-      loading: false,
-      data: action.payload
+    if (action.type === GET_USERS_FULFILLED) {
+        return {
+            ...state,
+            loading: false,
+            data: action.payload
+        }
     }
-  } 
 
-  if(action.type === GET_USER_FAIL){
-    return {
-      ...state,
-      loading: false,
-      error: action.error
+    if (action.type === GET_USER_FAIL) {
+        return {
+            ...state,
+            loading: false,
+            error: action.error
+        }
     }
-  }
 
-  if(action.type === GET_USERS_REJECTED){
-    return {
-      ...state,
-      loading: false,
-      error: action.payload.message
+    if (action.type === GET_USERS_REJECTED) {
+        return {
+            ...state,
+            loading: false,
+            error: action.payload.message
+        }
     }
-  }
-  
-  return state;
+
+    return state;
 }
 
-function sleep(ms){
-  return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      },ms);
-  });
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
 }
 
 // redux-thunk-middleware
 export function getUsersThunk() {
-  return async(dispatch, getState, {history}) => {
-    try {
-        console.log(history);
-          dispatch(getUsersStart());
-          //sleep
-          await sleep(2000);
-          const res = await axios.get('https://api.github.com/users');
-          dispatch(getUsersSuccess(res.data));
-          history.push('/');
-      } catch (error) {
-          dispatch(getUsersFail(error));
-      }
-  }
+    return async(dispatch, getState, {history}) => {
+        try {
+            console.log(history);
+            dispatch(getUsersStart());
+            //sleep
+            await sleep(2000);
+            const res = await axios.get('https://api.github.com/users');
+            dispatch(getUsersSuccess(res.data));
+            history.push('/');
+        } catch (error) {
+            dispatch(getUsersFail(error));
+        }
+    }
 }
-
 
 // redux-promise-middleware
 export function getUsersPromise() {
+    return {
+        type: GET_USERS,
+        payload: async() => {
+            const res = await axios.get('https://api.github.com/users');
+            return res.data;
+        }
+    }
+}
+
+// redux-saga
+const GET_USERS_SAGA_START = 'GET_USERS_SAGA_START';
+
+function* getUsersSaga(action) {
+    try {
+        yield put(getUsersStart());
+        // sleep 
+        yield delay(2000)
+        const res = yield call(axios.get, 'https://api.github.com/users')
+        yield put(getUsersSuccess(res.data));
+        yield put(push('/'))
+    } catch (error) {
+        yield put(getUsersFail(error));
+    }
+}
+
+export function getUsersSagaStart() {
   return {
-      type: GET_USERS,
-      payload: async() => {
-        const res = await axios.get('https://api.github.com/users');
-        return res.data;
-      }
+    type: GET_USERS_SAGA_START,
   }
+}
+
+export function* usersSaga() {
+  yield takeEvery(GET_USERS_SAGA_START, getUsersSaga)
 }
