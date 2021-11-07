be2bec4f6a599ba3e4e659f93ad69a9eebcdcac8
Stain-h <sangheon1646@naver.com>
2021-11-07 09:42:43 +0900

    modify : redux-actions 사용 2

--- src/redux/modules/filter.js
+++ src/redux/modules/filter.js
@@ -10,33 +10,3 @@ const reducer = handleActions({
 }, initialState, {prefix: 'redux-study/filter'})
 
 export default reducer;
-
-
-// ///////////////////////////////////// OLD_VER
-// // 액션 타입 
-// const SHOW_ALL = 'redux-study/filter/SHOW_ALL';
-// const SHOW_COMPLETE = 'redux-study/filter/SHOW_COMPLETE';
-
-// // 액션 생성 함수 
-// export function showAll() {
-//     return {type: SHOW_ALL}
-// }
-// export function showComplete() {
-//     return {type: SHOW_COMPLETE}
-// }
-
-// // 초기값
-// // {todos: [{text: '코딩', done: false}, {text: '점심먹기', done: false}], filter: 'ALL' }
-// const initialState = "ALL";
-
-// // 리듀서
-// export default function reducer(previousState = initialState, action) {
-//   if(action.type === SHOW_COMPLETE) {
-//       return 'COMPLETE';
-//   }
-//   if(action.type === SHOW_ALL) {
-//       return 'ALL';
-//   }
-
-//   return previousState; 
-// }
